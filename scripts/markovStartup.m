835b1cdc89799f6eb882c15f780d690ac35b8eda
Kristian holme <holme.kristian@gmail.com>
2023-10-04 14:04:58 +0200

    Update for markov

--- scripts/markovStartup.m
+++ scripts/markovStartup.m
@@ -1,7 +1,7 @@
 disp(['pwd: ', pwd()]);
-run("..\mrst-bitbucket\mrst-core\startup.m");
+run(".\..\..\..\mrst-bitbucket\mrst-core\startup.m");
 
-mrstPath register nfvm .\..\mrst-2023b\modules\nfvm;
-mrstPath register test-suite .\..\mrst-2023b\modules\test-suite
-mrstPath register ad-micp .\..\mrst-2023b\modules\ad-micp;
-mrstPath register prosjektOppgave \src\scripts
+mrstPath register nfvm .\..\..\..\mrst-2023b\modules\nfvm;
+mrstPath register test-suite .\..\..\..\mrst-2023b\modules\test-suite
+mrstPath register ad-micp .\..\..\..\mrst-2023b\modules\ad-micp;
+mrstPath register prosjektOppgave ./
