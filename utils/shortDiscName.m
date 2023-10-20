bb9ae249f5cdf8c30dff07b99cb141d9275d9044
Kristian holme <holme.kristian@gmail.com>
2023-10-20 19:24:19 +0200

    fix git

--- utils/shortDiscName.m
+++
@@ -1,8 +0,0 @@
-function discname = shortDiscName(discname)
-    if isempty(discname)
-        discname = 'tpfa';
-    else
-        discname = replace(discname, 'hybrid-', '');
-        discname = replace(discname, '-oo', '');
-    end
-end
