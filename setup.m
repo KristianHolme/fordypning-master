3d858bbe8768546449e25399eecbf4ed9598053c
Kristian holme <holme.kristian@gmail.com>
2023-11-20 12:17:57 +0100

    update remotesims and setup

--- setup.m
+++ setup.m
@@ -2,3 +2,4 @@ configFile = fileread('config.JSON');
 config = jsondecode(configFile);
 mrstPath('register', 'prosjektOppgave', fullfile(config.repo_folder, 'scripts'))
 mrstModule add prosjektOppgave
+mrstSettings('set', 'useMEX', true)
