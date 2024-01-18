7106704ee626f7abdb33fc2a075550a2507ff10b
Kristian Holme <holme.kristian@gmail.com>
2024-01-18 21:01:08 +0100

    update for markov

--- scripts/markovStartup.m
+++ scripts/markovStartup.m
@@ -14,5 +14,6 @@ mrstPath register nfvm /home/shomec/k/kholme/Documents/mrst-2023b/modules/nfvm;
 mrstPath register test-suite /home/shomec/k/kholme/Documents/mrst-2023b/modules/test-suite
 mrstPath register ad-micp /home/shomec/k/kholme/Documents/mrst-2023b/modules/ad-micp
 mrstPath register prosjektOppgave /home/shomec/k/kholme/Documents/Prosjektoppgave/src/scripts
+mrstPath register coarsegrid /home/shomec/k/kholme/Documents/mrst-2023b/modules/coarsegrid
 mrstModule add prosjektOppgave
 disp('markov startup done.');
