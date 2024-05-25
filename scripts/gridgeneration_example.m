9828abbbd6ca6cec34e17f4a0ace9854f6f3d25e
Kristian Holme <holme.kristian@gmail.com>
2024-05-25 22:35:28 +0200

    gridgeneration example

---
+++ scripts/gridgeneration_example.m
@@ -0,0 +1,15 @@
+%% Generate cut-cell grid with cartesian background grid and using pre-splitting instead of nudging
+G = GenerateCutCellGrid(130, 62, 'type', 'cartesian', 'presplit', true);
+figure;
+plotCellData(G, G.cells.tag);
+view(0,0);
+
+%% Generate PEBI-grid
+G = GeneratePEBIGrid(819, 117);
+
+%% Generate lower resolution PEBI-grid (not recomended) 
+G = GeneratePEBIGrid(350, 50, 'FCFactor', 0.4);
+plotCellData(G, G.cells.tag);
+view(0,0);
+set(gca, 'xlim', [3600, 4400], 'zlim', [250, 900]);
+% Observe that the resolution is very high along faults and horizons
