8d884b8592806d754f8901abe86a867052992cfe
Kristian Holme <holme.kristian@gmail.com>
2024-07-24 13:15:24 +0200

    updated setup after restructuring

--- setup.m
+++ setup.m
@@ -1,18 +1,19 @@
 configFile = fileread('config.JSON');
 config = jsondecode(configFile);
-mrstPath('register', 'masterthesis', fullfile(config.repo_folder, 'scripts'))
-mrstModule add masterthesis
+addpath(fullfile(config.repo_folder, 'scripts'))
+addpath(fullfile(config.repo_folder, 'src'))
+
 mrstSettings('set', 'useMEX', true)
 
 %%
 mrstModule add ad-core ad-props incomp mrst-gui mimetic linearsolvers ...
-    ad-blackoil postprocessing diagnostics masterthesis...
+    ad-blackoil postprocessing diagnostics...
     deckformat gmsh nfvm mpfa msrsb coarsegrid dfm libgeometry...
     upr jutul wellpaths sommer2024
 
 %% Make sure that the grid-files directory exists
 % Define the directory path
-gridPath = './grid-files';
+gridPath = './data/grid-files';
 
 % Check if the directory exists
 if ~exist(gridPath, 'dir')
