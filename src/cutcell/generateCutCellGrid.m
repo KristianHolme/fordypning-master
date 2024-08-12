0dd46f396c5110ad97d683b5eb132733fba42b22
Olav Møyner <olav.moyner@gmail.com>
2024-08-12 15:53:35 +0200

    Add mapping for background grid for case C

--- src/cutcell/generateCutCellGrid.m
+++ src/cutcell/generateCutCellGrid.m
@@ -304,6 +304,7 @@ end
 
 function G = addBackgroundGridMap(G, opt)
     if opt.backgroundGridMap
+        nz = 1;
         if strcmp(opt.SPEcase, 'A')
             % 280 x 120
             nx = 280;
@@ -316,11 +317,12 @@ function G = addBackgroundGridMap(G, opt)
             % 168 x 100 x 120
             nx = 168;
             ny = 100;
+            nz = 120;
             assert(strcmp(opt.SPEcase, 'C'))
         end
         [M, Gr, report] = getReductionMatrix(G, nx, ny);
         [I, J, V] = find(M);
-        G.reportingGrid = struct('map', [I, J, V], 'G', Gr);
+        G.reportingGrid = struct('map', [I, J, V], 'dims', [nx, ny, nz]);
     else
         G.reportingGrid = [];
     end
@@ -329,10 +331,40 @@ end
 function G = finalizeBackgroundGridCaseC(G, opt)
     if opt.backgroundGridMap
         assert(strcmp(opt.SPEcase, 'C'))
-        Gr = G.reportingGrid.G;
         map = G.reportingGrid.map;
         I = map(:, 1);
         J = map(:, 2);
-        K = map(:, 3);
+        W = map(:, 3);
+        assert(max(I) <= 16800)
+        assert(max(J) == G.layerSize)
+        layerI = {};
+        layerJ = {};
+        layerW = {};
+        sz_background = 120;
+        dy_rep = 1/sz_background;
+        dy_mesh = 1/opt.Cdepth;
+        for layerNo = 1:opt.Cdepth
+            J_offset = J + (layerNo-1)*G.layerSize;
+            x1 = (layerNo-1)*dy_mesh;
+            x2 = x1 + dy_mesh;
+            for layerNoBg = 1:sz_background
+                y1 = (layerNoBg-1)*dy_rep;
+                y2 = y1 + dy_rep;
+                is_overlapping = max(x1,y1) <= min(x2,y2);
+                overlap = min(x2,y2) - max(x1,y1);
+                if is_overlapping
+                    I_offset = I + (layerNoBg-1)*16800;
+                    assert(overlap >= 0)
+                    if overlap > 1e-10
+                        layerI{end+1} = I_offset;
+                        layerJ{end+1} = J_offset;
+                        layerW{end+1} = W.*overlap/dy_rep;
+                    end
+                else
+                    assert(overlap <= 0)
+                end
+            end
+        end
+        G.reportingGrid.map = [vertcat(layerI{:}), vertcat(layerJ{:}), vertcat(layerW{:})];
     end
 end
